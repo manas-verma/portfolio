fda37e4f7c49de54e68e7a75f9bca8ed992b414a
Manas Verma <manas.verma@gmail.com>
2023-09-21 14:23:57 -0500

    feat: adding build by tony fadell as book recommendation

--- src/portfolio/reading.ts
+++ src/portfolio/reading.ts
@@ -17,4 +17,12 @@ export const reading: ReadingItem[] = [
       "http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B074B2CZJG&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=manasverma0e-20&language=en_US",
     name: "Principles by Ray Dalio",
   },
+  {
+    id: "build",
+    tier: "A",
+    url: "https://www.amazon.com/Build-Unorthodox-Guide-Making-Things/dp/B09CF2YB6Z?crid=2MRQ0OS4OZN20&keywords=build+tony+fadell&qid=1695324094&sprefix=build+tony+fadel%2Caps%2C145&sr=8-1&linkCode=li2&tag=manasverma0e-20&linkId=cbf9b59a2426a315fb59f1ac0c98fa4a&language=en_US&ref_=as_li_ss_il",
+    image:
+      "http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B09CF2YB6Z&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=manasverma0e-20&language=en_US",
+    name: "Build by Tony Fadell",
+  },
 ];
