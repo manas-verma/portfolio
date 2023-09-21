3a5290e38f2b376347ffd7dc23d2bb23eb2d7717
Manas Verma <manas.verma@gmail.com>
2023-09-20 21:08:11 -0500

    feat: adding principles

--- src/portfolio/reading.ts
+++ src/portfolio/reading.ts
@@ -7,6 +7,14 @@ export const reading: ReadingItem[] = [
     url: "https://www.amazon.com/Creativity-Inc-Expanded-Overcoming-Inspiration/dp/B0BPF121ZJ?_encoding=UTF8&qid=1695260727&sr=8-1&linkCode=li2&tag=manasverma0e-20&linkId=7cf8b9cbe1ea75000477cf35ba97598f&language=en_US&ref_=as_li_ss_il",
     image:
       "http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B0BPF121ZJ&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=manasverma0e-20&language=en_US",
-    name: "Creativity Inc.",
+    name: "Creativity Inc. by Ed Catmull",
+  },
+  {
+    id: "principles",
+    tier: "S",
+    url: "https://www.amazon.com/Principles-Ray-Dalio-audiobook/dp/B074B2CZJG?crid=1PCFI9VI20IYF&keywords=Principles+ray+dalio&qid=1695262011&s=audible&sprefix=principles+ray+dalio%2Caudible%2C196&sr=1-1&linkCode=li2&tag=manasverma0e-20&linkId=01faa8a6bd257426078121bcc396d4cb&language=en_US&ref_=as_li_ss_il",
+    image:
+      "http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B074B2CZJG&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=manasverma0e-20&language=en_US",
+    name: "Principles by Ray Dalio",
   },
 ];
