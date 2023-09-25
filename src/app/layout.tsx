f190df018f5f336b23cf8383a3a10d1832bf0242
Manas Verma <manas.verma@gmail.com>
2023-09-24 20:14:49 -0500

    feat: update analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <Footer />
-      <Analytics />
+      <body className={inter.className}>
+        {children}
+        <Footer />
+        <Analytics />
+      </body>
     </html>
   );
 }
