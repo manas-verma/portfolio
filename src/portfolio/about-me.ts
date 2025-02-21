f684ac68064995ad78d3f55eb4220ffe1c2b8720
Manas Verma <manas.verma@gmail.com>
2025-02-21 13:23:09 +0600

    feat: update number of years

--- src/portfolio/about-me.ts
+++ src/portfolio/about-me.ts
@@ -3,7 +3,7 @@ import { trim } from "radash";
 export const aboutMe: string[] = [
   `Hello! I'm Manas, a software engineer based in Duluth, Minnesota. I'm currently a co-founder at WattShift.`,
   `I graduated from UC Berkeley in 2018 with a double in Physics and Computer Science.
-  I have five years of experience in software engineering -- both in big tech and in startups.`,
+  I have seven years of experience in software engineering -- both in big tech and in startups.`,
   `I'm originally from India, but I've moved around throughout my life, living in a total of 4
   different countries and 9 different cities (not including places I've just visited).
   In fact, I've never been in one place longer than four years.`,
