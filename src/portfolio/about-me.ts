8925fd3aa6855c267537635dc4799aea9c0c004c
Manas Verma <manas.verma@gmail.com>
2025-02-21 13:27:40 +0600

    feat: manually calculate years of experience

--- src/portfolio/about-me.ts
+++ src/portfolio/about-me.ts
@@ -1,9 +1,37 @@
 import { trim } from "radash";
 
+const yearsOfExperience = new Date().getFullYear() - 2018;
+const yearsOfExperienceStr = {
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  11: "eleven",
+  12: "twelve",
+  13: "thirteen",
+  14: "fourteen",
+  15: "fifteen",
+  16: "sixteen",
+  17: "seventeen",
+  18: "eighteen",
+  19: "nineteen",
+  20: "twenty",
+  21: "twenty-one",
+  22: "twenty-two",
+  23: "twenty-three",
+  24: "twenty-four",
+  25: "twenty-five",
+  26: "twenty-six",
+  27: "twenty-seven",
+  28: "twenty-eight",
+  29: "twenty-nine",
+  30: "thirty",
+};
+
 export const aboutMe: string[] = [
   `Hello! I'm Manas, a software engineer based in Duluth, Minnesota. I'm currently a co-founder at WattShift.`,
   `I graduated from UC Berkeley in 2018 with a double in Physics and Computer Science.
-  I have seven years of experience in software engineering -- both in big tech and in startups.`,
+  I have ${yearsOfExperienceStr} years of experience in software engineering -- both in big tech and in startups.`,
   `I'm originally from India, but I've moved around throughout my life, living in a total of 4
   different countries and 9 different cities (not including places I've just visited).
   In fact, I've never been in one place longer than four years.`,
